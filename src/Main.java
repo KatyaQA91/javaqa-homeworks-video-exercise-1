c49176af1aa63ac43f9e28ee086c68c96468fcd3
KatyaQA <ekaterina.rus91@mail.ru>
2023-04-19 22:07:17 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice;
+        int mileCost;
+        int totalMile;
+
+        ticketPrice = 3500;
+        mileCost = 20;
+        totalMile = ticketPrice / mileCost;
+
+        System.out.println("Вам начислено " + totalMile + " Бонусов");
+
+
+    }
+}
